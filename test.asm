9475d510d44a35ecb5e80ec710d127142a15e3d8
eBrnd <github@ebrnd.de>
2018-02-24 15:29:27 +0100

    clear the screen

--- test.asm
+++ test.asm
@@ -1,10 +1,23 @@
   processor 6502
   org $1000
 
-loop:
-  lda #$08
+  lda #$00 ; bläck
   sta $d020
-  sec
-  sbc #$08
   sta $d021
-  jmp loop
+
+  tax
+  lda #$20 ; späce
+
+  ; screen clear loop.
+  ; screen is from $0400 to $07ff
+  ; loops backwards from 255 to 0, then breaks
+loop:
+  sta $0400,x
+  sta $0500,x
+  sta $0600,x
+  sta $0700,x
+  dex
+  bne loop
+
+wait:
+  jmp wait
